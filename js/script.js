0580f0fb19958fceae318f5a74cd201a2301751c
pr-jack <prevgenij@gmail.com>
2019-09-24 10:13:33 +0200

    24.09.2019

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(".reviews-slider").slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
+
+$(".psychologists-2__slider").slick({
+  initialSlide: 1,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
+
+$(".comments__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  //autoplay: true,
+  autoplaySpeed: 3000
+});
